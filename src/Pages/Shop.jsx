a76a09d160daa892caa2754caeaa29b80962eb44
pranavashish <pranavashish@gmail.com>
2024-03-13 11:21:32 +0530

    first commit

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+
+const Shop = () => {
+  const [popular, setPopular] = useState([]);
+  const [newcollection, setNewCollection] = useState([]);
+
+  const fetchInfo = () => {
+    fetch("https://e-commerce-backend-1-s3dg.onrender.com/popularinwomen")
+      .then((res) => res.json())
+      .then((data) => setPopular(data));
+    fetch("https://e-commerce-backend-1-s3dg.onrender.com/newcollections")
+      .then((res) => res.json())
+      .then((data) => setNewCollection(data));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div>
+      <Hero />
+      <Popular data={popular} />
+      <Offers />
+      <NewCollections data={newcollection} />
+      <NewsLetter />
+    </div>
+  );
+};
+
+export default Shop;
